34f59f24a824b2346fada0c269c19b9f9cad94a8
PC <dijanic@gmail.com>
2025-01-05 16:33:17 +0100

    * Added functionality for adding tasks

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,16 @@
   <p v-else>User is not 18</p>
 
   <ol>
-    <li v-for="singleClass in classes" :key="singleClass">{{ singleClass }}</li>
+    <li v-on:click="deleteClass(singleClass)" v-for="singleClass in classes" :key="singleClass">{{ singleClass }}</li>
   </ol>
 
   <form @submit.prevent>
     <input v-model="name" type="text" placeholder="Unesite vase ime">
+  </form>
+
+  <form @submit.prevent>
+    <input v-model="className" type="text" placeholder="Unesite ime jezika">
+    <input v-on:click="addClass" type="button" value="Dodaj jezik">
   </form>
 
   <button v-on:click="changeUserType">Promeni tipa korisnika</button>
@@ -28,12 +33,21 @@
         name: "Toma",
         professor: false,
         age: 11,
-        classes: ['Javascript', 'VueJS', 'ReactJS', 'NodeJS']
+        classes: ['Javascript', 'VueJS', 'ReactJS', 'NodeJS'],
+        className: null,
       }
     },
     methods: {
       changeUserType() {
         this.professor = !this.professor;
+      },
+      deleteClass(singleClass) {
+        let classIndex = this.classes.indexOf(singleClass); // 0, 1, 2, 3
+        this.classes.splice(classIndex, 1);
+      },
+      addClass() {
+        this.classes.push(this.className);
+        this.className = null;
       }
     }
   }
